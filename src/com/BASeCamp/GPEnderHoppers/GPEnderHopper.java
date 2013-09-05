a6a321d67d2e7e470a05fe977e5fb2e74267020a
Oliver Wright <meiamsome@hotmail.com>
2013-09-05 15:37:32 +0100

    Fix formatting & remove unused imports
    Now on Bukkit's formatting

--- src/com/BASeCamp/GPEnderHoppers/GPEnderHopper.java
+++ src/com/BASeCamp/GPEnderHoppers/GPEnderHopper.java
@@ -1,4 +1,5 @@
 package com.BASeCamp.GPEnderHoppers;
+
 import java.util.logging.Logger;
 
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
@@ -6,57 +7,52 @@ import nl.rutgerkok.betterenderchest.BetterEnderChest;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.Configuration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
 public class GPEnderHopper extends JavaPlugin {
-	
-	GriefPrevention gp = null;
-	HopperHandler hh = null;
-	HopperCommand hc = null;
-	public BetterEnderChest becPlugin = null;
-	public static Logger log = Logger.getLogger("Minecraft");
-	public Configuration config;
-	
-	
-	@Override
-	public void onDisable(){
-		if(hh!=null)
-			hh.saveData();
-	}
-	
-	@Override
-	public void onEnable(){
-		log.info("GPEnderHopper Loading...");
-		try {
-			gp = (GriefPrevention) Bukkit.getPluginManager().getPlugin("GriefPrevention");
-		} catch(Exception e) {
-			log.severe("Found a GriefPrevention plugin but it is not of the right class!");
-		}
-		if(gp == null) {
-			log.severe("GPEnderHopper: Could not find GriefPrevention. Disabling!");
-			getPluginLoader().disablePlugin(this);
-			return;
-		}
-		try {
-			becPlugin = (BetterEnderChest) Bukkit.getPluginManager().getPlugin("BetterEnderChest");
-		} catch(Exception e) {
-			log.severe("Found a GriefPrevention plugin but it is not of the right class!");
-		}
-		saveDefaultConfig();
-		config = getConfig();
-		
-		//register for Hopper Events.
-		hh = new HopperHandler(this);
-		Bukkit.getPluginManager().registerEvents(hh, this);
-		
-		hc = new HopperCommand(this);
-		getCommand("claimecpull").setExecutor(hc);
-		getCommand("claimecpush").setExecutor(hc);
-		
-		
-	}
-	
+
+    GriefPrevention gp = null;
+    HopperHandler hh = null;
+    HopperCommand hc = null;
+    public BetterEnderChest becPlugin = null;
+    public static Logger log = Logger.getLogger("Minecraft");
+    public Configuration config;
+
+    @Override
+    public void onDisable() {
+        if (hh != null)
+            hh.saveData();
+    }
+
+    @Override
+    public void onEnable() {
+        log.info("GPEnderHopper Loading...");
+        try {
+            gp = (GriefPrevention) Bukkit.getPluginManager().getPlugin("GriefPrevention");
+        } catch (Exception e) {
+            log.severe("Found a GriefPrevention plugin but it is not of the right class!");
+        }
+        if (gp == null) {
+            log.severe("GPEnderHopper: Could not find GriefPrevention. Disabling!");
+            getPluginLoader().disablePlugin(this);
+            return;
+        }
+        try {
+            becPlugin = (BetterEnderChest) Bukkit.getPluginManager().getPlugin("BetterEnderChest");
+        } catch (Exception e) {
+            log.severe("Found a GriefPrevention plugin but it is not of the right class!");
+        }
+        saveDefaultConfig();
+        config = getConfig();
+
+        // register for Hopper Events.
+        hh = new HopperHandler(this);
+        Bukkit.getPluginManager().registerEvents(hh, this);
+
+        hc = new HopperCommand(this);
+        getCommand("claimecpull").setExecutor(hc);
+        getCommand("claimecpush").setExecutor(hc);
+
+    }
 
 }
--- src/com/BASeCamp/GPEnderHoppers/HopperCommand.java
+++ src/com/BASeCamp/GPEnderHoppers/HopperCommand.java
@@ -1,106 +1,79 @@
 package com.BASeCamp.GPEnderHoppers;
 
-import java.security.Permission;
-import java.util.ArrayList;
-import java.util.List;
-
 import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import me.ryanhamshire.GriefPrevention.Messages;
 import me.ryanhamshire.GriefPrevention.PlayerData;
 import me.ryanhamshire.GriefPrevention.TextMode;
-import nl.rutgerkok.betterenderchest.BetterEnderChest;
-import nl.rutgerkok.betterenderchest.BetterEnderChestPlugin.PublicChest;
-import nl.rutgerkok.betterenderchest.chestprotection.ProtectionBridge;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class HopperCommand implements CommandExecutor{
+public class HopperCommand implements CommandExecutor {
 
-	
-	
-	private GPEnderHopper Owner = null;
-	public GPEnderHopper getOwner() { return Owner;}
-	
-	
-	public HopperCommand(GPEnderHopper pOwner){
-		Owner = pOwner;
-	}
-	
-	
-	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2,
-			String[] arg3) {
-		// TODO Auto-generated method stub
-		if(!(arg0 instanceof Player)) return false;
-		Player p = (Player)arg0;
-		Claim inclaim = Owner.gp.dataStore.getClaimAt(p.getLocation(), true, null);
-		
-		
-		
-		
-		
-		if(arg2.equalsIgnoreCase("claimecpull") ||
-				(arg2.equalsIgnoreCase("claimecpush"))){
-			if(inclaim==null){
-				p.sendMessage(ChatColor.RED + " You must be inside a claim to change it!");
-				return false;
-			}
-			boolean ignoreclaimperm = p.hasPermission("GriefPrevention.IgnoreClaims");
-			PlayerData pd = Owner.gp.dataStore.getPlayerData(p.getName());
-			
-			
-			
-			
-			//make sure the claim is owned by the player, or they are set to ignore claims.
-			if(!(pd.ignoreClaims   || inclaim.ownerName.equalsIgnoreCase(p.getName()))){
-				//nope!
-				p.sendMessage(ChatColor.RED + " You do not own this claim!");
-				//if they have ignoreclaims perm from GP, show that...
-				if(ignoreclaimperm){
-					GriefPrevention.sendMessage(p,TextMode.Info,Messages.IgnoreClaimsAdvertisement);
-					
-				}
-				return false;
-				
-			}
-			
-			
-				//checks out, toggle appropriate perm.
-				//retrieve the claim Data.
-				ClaimData cd = Owner.hh.getHopperData(inclaim);
-				if(arg2.equalsIgnoreCase("claimecpull")){
-					cd.setHopperPull(!cd.getHopperPull());
-					p.sendMessage("Hopper Enderchest Pull:" + (cd.getHopperPull()?"On":"Off"));
-					cd.Save();
-				}
-				else if(arg2.equalsIgnoreCase("claimecpush")){
-					cd.setHopperPush(!cd.getHopperPush());
-					p.sendMessage("Hopper Enderchest Push:" + (cd.getHopperPush()?"On":"Off"));
-					cd.Save();
-				}
-				//save it.
-				cd.Save();
-			
-			
-			    return true;
-			
-		}
-		
-	
-		
-		
-		return false;
-	}
-	
-	
-	
+    private GPEnderHopper Owner = null;
+
+    public GPEnderHopper getOwner() {
+        return Owner;
+    }
+
+    public HopperCommand(GPEnderHopper pOwner) {
+        Owner = pOwner;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+        // TODO Auto-generated method stub
+        if (!(arg0 instanceof Player))
+            return false;
+        Player p = (Player) arg0;
+        Claim inclaim = Owner.gp.dataStore.getClaimAt(p.getLocation(), true, null);
+
+        if (arg2.equalsIgnoreCase("claimecpull") || (arg2.equalsIgnoreCase("claimecpush"))) {
+            if (inclaim == null) {
+                p.sendMessage(ChatColor.RED + " You must be inside a claim to change it!");
+                return false;
+            }
+            boolean ignoreclaimperm = p.hasPermission("GriefPrevention.IgnoreClaims");
+            PlayerData pd = Owner.gp.dataStore.getPlayerData(p.getName());
+
+            // make sure the claim is owned by the player, or they are set to
+            // ignore claims.
+            if (!(pd.ignoreClaims || inclaim.ownerName.equalsIgnoreCase(p.getName()))) {
+                // nope!
+                p.sendMessage(ChatColor.RED + " You do not own this claim!");
+                // if they have ignoreclaims perm from GP, show that...
+                if (ignoreclaimperm) {
+                    GriefPrevention.sendMessage(p, TextMode.Info, Messages.IgnoreClaimsAdvertisement);
+
+                }
+                return false;
+
+            }
+
+            // checks out, toggle appropriate perm.
+            // retrieve the claim Data.
+            ClaimData cd = Owner.hh.getHopperData(inclaim);
+            if (arg2.equalsIgnoreCase("claimecpull")) {
+                cd.setHopperPull(!cd.getHopperPull());
+                p.sendMessage("Hopper Enderchest Pull:" + (cd.getHopperPull() ? "On" : "Off"));
+                cd.Save();
+            } else if (arg2.equalsIgnoreCase("claimecpush")) {
+                cd.setHopperPush(!cd.getHopperPush());
+                p.sendMessage("Hopper Enderchest Push:" + (cd.getHopperPush() ? "On" : "Off"));
+                cd.Save();
+            }
+            // save it.
+            cd.Save();
+
+            return true;
+
+        }
+
+        return false;
+    }
 
 }
